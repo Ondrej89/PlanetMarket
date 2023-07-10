9b2a109b496b9c6b348793db7d128b24e1c8cd46
Ondrej89 <ondrejdudas89@gmail.com>
2023-07-10 22:17:27 +0200

    ...

--- lib/assets.ts
+++ lib/assets.ts
@@ -1,5 +1,4 @@
 export const images = [
-  "https://uploadthing.com/f/afe0d65b-df4c-4feb-83e0-e3cd00c5a6cb_lindsay-henwood-7_kRuX1hSXM-unsplash-min.jpg",
-  "https://uploadthing.com/f/4fd84b7c-cf5c-4ca0-a381-34d99a307429_nik-korba-3WceTBlUoMs-unsplash-min.jpg",
-  "https://uploadthing.com/f/5cf2b3c6-c116-447b-8180-0db55c529393_nong-v-iad87c3bgwU-unsplash-min.jpg",
+  "https://uploadthing.com/f/a1fafdcd-6d76-4b7d-8069-696b25485824_Baner2.jpg",
+  "https://uploadthing.com/f/8e7f89f2-6f33-4b83-a1e6-568faf670064_Baner1.jpg",
 ];
